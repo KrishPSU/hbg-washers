0128b119730e355ed757b058df2362da1e2b8a7c
KrishPSU <163033777+KrishPSU@users.noreply.github.com>
2025-04-11 04:34:02 -0400

    Create load.js

---
+++ app/js/load.js
@@ -0,0 +1,27 @@
+const socket = io();
+
+
+
+socket.on('all-machine-status', (data) => {
+    console.log(data);
+
+
+    for (let i=0; i<data.length; i++) {
+        let machine = data[i];
+        for (let j=0; j<book_btns.length; j++) {
+            let btn = book_btns[j];
+            if (machine.id == btn.getAttribute('data-machine-id')) {
+                btn.setAttribute('data-machine-status', data[i].available);
+                if (machine.available) {
+                    btn.innerText = "Available";
+                    btn.classList.remove("booked");
+                    btn.disabled = false;
+                } else {
+                    btn.innerText = "Booked";
+                    btn.classList.add("booked");
+                    btn.disabled = true;
+                }
+            }
+        }
+    }
+});
